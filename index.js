991e03b7e750c4920a6a387b2c79ad4eb91ec892
dhirajkahar <dhirajkahar7@gmail.com>
2021-06-09 01:33:18 +0530

    metrics

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send('About page')
-  })
+})
+
+app.get('/html', (req, res) => {
+    res.send('<a href="https://www.w3schools.com">Visit W3Schools.com!</a> ');
+})
+
+app.get('/redirect', (req, res) => {
+    res.redirect(301, '/');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
