745d83d8c17736b9af906df09064081e51ad3239
dhiraj kahar <32600353+cooldhiraj@users.noreply.github.com>
2021-06-08 17:37:57 +0530

    Update index.js
    Integrated Express

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 const tracer = require('dd-trace').init({
     logInjection: true
 });
-
-
+var dd_options = {
+  'response_code':true,
+  'tags': ['app:heroku-express']
+ }
+var connect_datadog = require('connect-datadog')(dd_options);
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
 express()
     .use(express.static(path.join(__dirname, 'public')))
+    .use(connect_datadog)
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
